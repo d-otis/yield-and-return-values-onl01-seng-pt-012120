2100cce324dc1c50457ace47ead902e447fdd963
Dan Foley <dan@dan-foley.com>
2020-01-24 01:25:30 +0000

    Automatically backed up by Learn

--- lib/practicing_returns.rb
+++ lib/practicing_returns.rb
@@ -2,14 +2,13 @@ require 'pry'
 
 def hello(array)
   i = 0
+  collection = []
   while i < array.length
-  # binding.pry
     yield(array[i])
     i += 1
   end
   array
 end
 
-binding.pry
 # hello(["Tim", "Tom", "Jim"]) { |name| puts "Hi, #{name}" }
 hello(["Tim", "Tom", "Jim"]) { |name| "Hi, #{name}" }
